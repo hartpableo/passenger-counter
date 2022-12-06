f40b3896dda96f4776bc9bb578f35805da90183a
Hart <pableoh@gmail.com>
2022-12-06 09:32:22 +0800

    add task functionality complete!

--- js/script.js
+++ js/script.js
@@ -19,6 +19,24 @@ inputField.addEventListener('input', () => {
     manipulateButton();
 });
 
+// localstorage data
+(localStorage.getItem('tasks') == null) ? localStorage.setItem('tasks', '[]') : null;
+let initialData = JSON.parse(localStorage.getItem('tasks'));
+
+// get and display data from localstorage
+const displayTasks = (item) => {
+        let taskListItemMarkup = `<li class="task-list-item list-group-item d-flex flex-wrap flex-md-nowrap justify-content-between align-items-start">
+        <span class="text-center text-md-start">${item}</span>
+        <div class="task-controls text-md-end text-center text-white mt-3 mt-md-0">
+            <button class="complete d-inline-flex justify-content-center align-items-center align-middle" type="button"><small class="visually-hidden">Task Completed</small><i class="bi bi-check-lg"></i></button>
+            <button class="delete d-inline-flex justify-content-center align-items-center align-middle" type="button"><small class="visually-hidden">Delete Task</small><i class="bi bi-trash"></i></button>
+            <button class="edit d-inline-flex justify-content-center align-items-center align-middle" type="button"><small class="visually-hidden">Edit Task</small><i class="bi bi-pencil-square"></i></button>
+        </div>
+        </li>`;
+        taskList.insertAdjacentHTML('beforeend', taskListItemMarkup);
+}
+initialData.forEach(displayTasks);
+
 // task list section display
 const listItemCount = taskListItem.length;
 const removeTaskList = () => {
@@ -37,21 +55,23 @@ const displayTaskList = () => {
     emptyListNotice.classList.remove('d-block');
     emptyListNotice.classList.add('d-none');
 };
-const taskListSectionDisplay = (listItemCount === 0) ? removeTaskList() : displayTaskList();
+const taskListSectionDisplay = (listItemCount === 0 && initialData.length === 0) ? removeTaskList() : displayTaskList();
 
 // append inputted data to the list
 taskForm.addEventListener('submit', (e) => {
     let inputFieldValue = inputField.value.trim();
-    let taskListItemMarkup = `<li class="task-list-item list-group-item d-flex justify-content-between align-items-start">
-<span>${inputFieldValue}</span>
-<div class="task-controls text-end text-white">
-    <button class="complete d-inline-flex justify-content-center align-items-center align-middle" type="button"><small class="visually-hidden">Task Completed</small><i class="bi bi-check-lg"></i></button>
-    <button class="delete d-inline-flex justify-content-center align-items-center align-middle" type="button"><small class="visually-hidden">Delete Task</small><i class="bi bi-trash"></i></button>
-    <button class="edit d-inline-flex justify-content-center align-items-center align-middle" type="button"><small class="visually-hidden">Edit Task</small><i class="bi bi-pencil-square"></i></button>
-</div>
-</li>`;
+    initialData.push(inputFieldValue);
+    let taskListItemMarkup = `<li class="task-list-item list-group-item d-flex flex-wrap flex-md-nowrap justify-content-between align-items-start">
+    <span class="text-center text-md-start">${inputFieldValue}</span>
+    <div class="task-controls text-md-end text-center text-white mt-3 mt-md-0">
+        <button class="complete d-inline-flex justify-content-center align-items-center align-middle" type="button"><small class="visually-hidden">Task Completed</small><i class="bi bi-check-lg"></i></button>
+        <button class="delete d-inline-flex justify-content-center align-items-center align-middle" type="button"><small class="visually-hidden">Delete Task</small><i class="bi bi-trash"></i></button>
+        <button class="edit d-inline-flex justify-content-center align-items-center align-middle" type="button"><small class="visually-hidden">Edit Task</small><i class="bi bi-pencil-square"></i></button>
+    </div>
+    </li>`;
     taskList.insertAdjacentHTML('beforeend', taskListItemMarkup);
     displayTaskList();
+    localStorage.setItem('tasks', JSON.stringify(initialData));
     e.preventDefault();
     inputField.value = '';
     manipulateButton();
